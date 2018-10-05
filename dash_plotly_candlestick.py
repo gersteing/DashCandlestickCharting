1079ffe0060106ef2eb34b741996b15808fe8a53
Garth Gerstein <sintrbug@gmail.com>
2018-10-05 10:06:02 -0400

    Update comments

--- dash_plotly_candlestick.py
+++ dash_plotly_candlestick.py
@@ -28,15 +28,16 @@ def utc_milleseconds_to_date(milliseconds):
     global localTimeZone
     seconds = milliseconds/1000
     ##datetime.fromtimestamp seems to return a time stamp that was four hours off the time stamp of my actual data.
-    ##I think the function returns a naive object but I am not clear what time zone is assumed. I thought it was utc
+    ##I think the function returns a naive date time object but I am not clear what time zone is assumed. I thought it was utc
     ##but I am in US/Eastern time where the offset should be -5 hours for east coast time, not -4 hours.
-    ##Long story short,to make the timetamps works wiht the graph, every time I convert a timestamp from the slider
-    ##to a date, I needed to convert it to the timezone of the actual bar data. In my case, that was east coast time. 
+    ##Long story short,to make the timetamps works with the graph, every time I convert a timestamp from the slider
+    ##to a datetime object, I needed to localize the time (adjust the datetime object) it to the timezone used when for actual bar data
+    ##which in my case was US/Eastern time.  
     ##Otherwise the date range we assign to the chart will not match the actual datetime of our bar data and our y range
     ##values do not get calculated correctly.
     parsedDateTime = datetime.fromtimestamp(seconds)
-    ##convert this object back to our local time zone because my data time stamps were in east coast time.
-    ##Adjust localTimeZone if you are not on east coast time.
+    ##localize this object back to time zone of the source bar data. My data time stamps were in east coast time.
+    ##Adjust the localTimeZone variable to what ever your data time zone is if you are not on east coast time.
     d_aware =  parsedDateTime.astimezone(localTimeZone)
     return d_aware
 
